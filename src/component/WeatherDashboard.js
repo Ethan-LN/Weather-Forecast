7542c9134dad516808307d2e4f0ab75cc9c316b9
Ethan <et.l.nicholas@gmail.com>
2023-07-18 20:47:13 +1000

    temp save before processing data

--- src/component/WeatherDashboard.js
+++ src/component/WeatherDashboard.js
@@ -1,27 +1,42 @@
 import { forecastData } from "../WeatherData";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { WeatherDisplay } from "./WeatherDisplay";
 import DailyLineChart from "./DailyLineChart";
-import "../styles/WeatherDashboard.css"
-
+import "../styles/WeatherDashboard.css";
 
 
 export const WeatherDashboard = () => {
+  const [hourlyData, setHourlyData] = useState("");
+  const [hourlyDate, setHourlyDate] = useState("monday");
+  useEffect(()=>{
+    forecastData.map((data) => {
+      if(data.day===hourlyDate) {
+        setHourlyData(data.hourlyTemp)
+        console.log("this is hourly data "+ hourlyData + data.day);
+      }
+    });
+  },[hourlyDate,hourlyData])
+
+
   return (
     <div className="title">
       <h2>Weather Forecast</h2>
       <div className="cards__panel">
         {forecastData.map((data) => (
           <WeatherDisplay
+            key={data.id}
+            id={data.id}
             day={data.day}
             conditions={data.conditions}
             maxTemp={data.maxTemp}
             wind={data.wind}
+            hourlyDate={hourlyDate}
+            setHourlyDate={setHourlyDate}
           />
         ))}
       </div>
       <div className="hourly__display">
-      <DailyLineChart />
+        <DailyLineChart/>
       </div>
     </div>
   );
--- src/component/WeatherDisplay.js
+++ src/component/WeatherDisplay.js
@@ -3,29 +3,53 @@ import "../styles/WeatherDisplay.css";
 
 export const WeatherDisplay = (props) => {
   const weatherCondition = {
-    "sunny": "☀️",
-    "cloudy": "🌥",
-    "stormy": "⛈",
-    "rainy": "🌧"
-  }
+    sunny: "☀️",
+    cloudy: "🌥",
+    stormy: "⛈",
+    rainy: "🌧",
+  };
   const celsuis = String.fromCharCode(176) + "C";
   const speedUnit = "km/h";
+  const displayHourlyData = () => {
+    props.setHourlyDate(props.day);
+  };
   return (
-    <div className="card__body">
-      <div className="card__contents">
-        <div>{props.day.substring(0, 3)}</div>
-        <div className="condition__indicator">{weatherCondition[props.conditions]}</div>
-        <div>
-          {props.conditions.charAt(0).toUpperCase()}
-          {props.conditions.slice(1)}
+    <div className="card__frame">
+      <div className="card__body">
+        <div className="card__contents">
+          <div>{props.day.substring(0, 3)}</div>
+          <div className="condition__indicator">
+            {weatherCondition[props.conditions]}
+          </div>
+          <div>
+            {props.conditions.charAt(0).toUpperCase()}
+            {props.conditions.slice(1)}
+          </div>
+          <div>
+            Max {props.maxTemp}
+            {celsuis}
+          </div>
+          <div>
+            {props.wind}
+            {speedUnit}
+          </div>
+          <div className="heat__indicator">
+            {" "}
+            {props.maxTemp >= 35 ? "Heatwave warning" : ""}
+          </div>
+          <div className="wind__indicator">
+            {" "}
+            {props.wind >= 30 ? "Strong wind warning" : ""}
+          </div>
+          <div className="btn__hourly__body">
+            <button
+              className="btn__hourly__content"
+              onClick={displayHourlyData}
+            >
+              Hourly
+            </button>
+          </div>
         </div>
-        <div>
-          Max {props.maxTemp}
-          {celsuis}
-        </div>
-        <div>{props.wind}{speedUnit}</div>
-        <div className="heat__indicator"> {props.maxTemp >= 35 ?"Heatwave warning" : ""}</div>
-        <div className="wind__indicator"> {props.wind >= 30 ? "Strong wind warning" : ""}</div>
       </div>
     </div>
   );
