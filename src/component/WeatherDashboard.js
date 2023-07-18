d6eb3744981a70ef7c40b23fa6d97091013c532d
Ethan <et.l.nicholas@gmail.com>
2023-07-18 15:42:39 +1000

    adjust the card and add line graph for hourly temp

--- src/component/WeatherDashboard.js
+++ src/component/WeatherDashboard.js
@@ -1,22 +1,28 @@
 import { forecastData } from "../WeatherData";
 import React from "react";
 import { WeatherDisplay } from "./WeatherDisplay";
-import '../styles/WeatherDashboard.css'
+import DailyLineChart from "./DailyLineChart";
+import "../styles/WeatherDashboard.css"
+
+
 
 export const WeatherDashboard = () => {
   return (
     <div className="title">
-    <h2>Weather Forecast</h2>
-    <div className="cards__panel">     
-      {forecastData.map((data) => (
-        <WeatherDisplay
-          day={data.day}
-          conditions={data.conditions}
-          maxTemp={data.maxTemp}
-          wind={data.wind}
-        />
-      ))}
-    </div>
+      <h2>Weather Forecast</h2>
+      <div className="cards__panel">
+        {forecastData.map((data) => (
+          <WeatherDisplay
+            day={data.day}
+            conditions={data.conditions}
+            maxTemp={data.maxTemp}
+            wind={data.wind}
+          />
+        ))}
+      </div>
+      <div className="hourly__display">
+      <DailyLineChart />
+      </div>
     </div>
   );
 };
--- src/component/WeatherDisplay.js
+++ src/component/WeatherDisplay.js
@@ -2,19 +2,25 @@ import React from "react";
 import "../styles/WeatherDisplay.css";
 
 export const WeatherDisplay = (props) => {
+  const weatherCondition = {
+    "sunny": "☀️",
+    "cloudy": "🌥",
+    "stormy": "⛈",
+    "rainy": "🌧"
+  }
   const celsuis = String.fromCharCode(176) + "C";
-  const speedUnit = "km/h"
+  const speedUnit = "km/h";
   return (
     <div className="card__body">
       <div className="card__contents">
-        <div>☀️</div>
         <div>{props.day.substring(0, 3)}</div>
+        <div className="condition__indicator">{weatherCondition[props.conditions]}</div>
         <div>
           {props.conditions.charAt(0).toUpperCase()}
           {props.conditions.slice(1)}
         </div>
         <div>
-          {props.maxTemp}
+          Max {props.maxTemp}
           {celsuis}
         </div>
         <div>{props.wind}{speedUnit}</div>
